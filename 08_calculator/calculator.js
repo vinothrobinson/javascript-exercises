3182c1b9157e93db1e42674432a44305f9e3c8f2
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-28 14:41:52 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+	const totalSum = array.reduce((total, item) => {
+    return total + (item);
+  });
+  return totalSum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+	const totalSum = array.reduce((total, item) => {
+    return total * (item);
+  });
+  return totalSum
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number**power
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1
+  }
+  if (number === 1) {
+    return 1
+  }
+  else {
+    return number * factorial(number-1)
+  }
 };
 
 // Do not edit below this line
