7d7299ac69a9252075714a68eeb16675e391c301
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-28 16:13:46 -0400

    Compelte exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(object) {
+    const ordered = object.sort(function(a, b){
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = (new Date()).getFullYear();
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        const personOneAge = a.yearOfDeath - a.yearOfBirth;
+        const personTwoAge = b.yearOfDeath - b.yearOfBirth
+        if (personOneAge > personTwoAge) {
+            return -1;
+        }
+        else return 1;
+    });
+    console.log(ordered)
+    return ordered[0]
 };
 
 // Do not edit below this line
