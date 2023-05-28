49ff70612c157669c6005a3eaa8e194a4b7c09a1
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-28 15:48:56 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof(number) === String) {
+        number = number.toString()
+    }
+    if (number < 0) {
+        return "OOPS"
+    }
+    const fibArray = [];
+    for (let i = 0; i < number; i++) {
+        fibArray[i] = createFib(i)
+    };
+    console.log(fibArray);
+    return fibArray[fibArray.length-1]
+}
 
-};
-
+function createFib(number) {
+    if (number === 0) {
+        return 1
+      }
+      if (number === 1) {
+        return 1
+      }
+      else {
+        return createFib(number-1) + createFib(number-2)
+      }
+}
 // Do not edit below this line
 module.exports = fibonacci;
