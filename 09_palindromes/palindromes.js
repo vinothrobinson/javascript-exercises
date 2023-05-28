f68b674bce2ed68f2adaf23b686f1fa0aac3263c
vinothrobinson <vinothrobinson03@gmail.com>
2023-05-28 15:31:20 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let symbols = ['!', ',', '.', ' '];
+    const punctuation = /[\.,?!]/g;
+    let newString = string.replace(punctuation, "");
+    for (let letter = 0; letter < newString.length; letter++){
+        if (newString[letter] === " ") {
+            newString = newString.replace(newString[letter], '');
+        }
+    }
+    newString = newString.toLowerCase();
+    console.log(newString);
+    for (let start = 0; start < newString.length; start++){
+        let end = newString.length - (start+1);
+        if (newString[start] !== newString[end]){
+            return false
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
